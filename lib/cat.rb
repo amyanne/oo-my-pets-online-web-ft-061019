a01be59e4ab1994eb03c6f753ba891681b1d0e5c
Amy Westcott <amy.westcott@yahoo.com>
2019-06-27 15:56:19 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+ attr_accessor :mood
+ attr_reader :name
+  @@all = []
+  def initialize(name, cat_owner = nil, mood = "nervous")
+    @cat_owner = cat_owner
+    @name = name
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  @@all = []
+  def initialize(name, dog_owner = nil, mood ="nervous")
+    @dog_owner = dog_owner
+    @name = name
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+ 
 end
