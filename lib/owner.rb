9b3b4c33a95e8817974ae04a3221742966199ec7
Amy Westcott <amy.westcott@yahoo.com>
2019-06-27 15:58:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,9 +55,9 @@ class Owner
    end 
    
    def sell_pets
-     Cat.all.each {|c| c.mood = "nervous"}
+     Cat.all.each {|c| c.mood = "nervous", c.owner = nil}
      Dog.all.each {|d| d.mood = "nervous"}
-     pets.clear
+    
    end 
   def list_pets
    "I have #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
